3818b4bae83bf2c2e4ac5d321e3b41adae91168c
li <li@lidemacbook-air.wireless-1x.unc.edu>
2022-11-02 13:48:05 -0400

    revise

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ import minimist from 'minimist'
 const args = minimist(process.argv.slice(2))
 
 let sides = args.sides && args.sides > 0 ? args.sides: 6;
-let dice = args.dice && args.sides ? args.dice: 2;
-let rolls = args.rolls && args.sides ? args.rolls: 1;
+let dice = args.dice && args.dice>0 ? args.dice: 2;
+let rolls = args.rolls && args.rolls>0 ? args.rolls: 1;
 
 console.log(JSON.stringify(roll(sides, dice, rolls)));
